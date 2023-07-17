d769545ce553ac3e4c1b42b1ef7dd3137cd20b76
Piotr Olszewski <asmie@asmie.pl>
2023-07-17 16:23:08 +0200

    feat: loadable pallet with empty execute extrinsic
    This commit introduces truly working pallet that can be loaded into
    the Substrate runtime.
    
    Pallet can be loaded and execute function can be run which is emitting
    event about the fact it has been called.
    
    Weight file has been generated automatically as weights are needed
    for the extrinsics to work.

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+pub mod weights;
+pub use weights::*;
+
+// The pallet is defined below.
+#[frame_support::pallet]
+pub mod pallet {
+    use super::*;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    /// MoveVM pallet configuration trait
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// Because this pallet emits events, it depends on the runtime's definition of an event.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+        /// Type representing the weight of this pallet
+        type WeightInfo: WeightInfo;
+    }
+
+    // Pallets use events to inform users when important changes are made.
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// Event about calling execute function.
+        /// [account]
+        ExecuteCalled { who: T::AccountId },
+    }
+
+    // Dispatchable functions allows users to interact with the pallet and invoke state changes.
+    // These functions materialize as "extrinsics", which are often compared to transactions.
+    // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// Execute function
+        #[pallet::call_index(0)]
+        #[pallet::weight(T::WeightInfo::execute())]
+        pub fn execute(origin: OriginFor<T>) -> DispatchResult {
+            // Allow only signed calls.
+            let who = ensure_signed(origin)?;
+
+            // Emit an event.
+            Self::deposit_event(Event::ExecuteCalled { who });
+            
+            // Return a successful DispatchResult
+            Ok(())
+        }
+
+    }
+}
