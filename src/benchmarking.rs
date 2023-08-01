10620416d56ce49520e5178b936f29cfdad4daed
Piotr Olszewski <asmie@asmie.pl>
2023-08-01 11:05:03 +0200

    feat: added publishing modules - extrinsics take bytecode and gas parameters

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -14,8 +14,17 @@ mod benchmarks {
     #[benchmark]
     fn execute() {
         let caller: T::AccountId = whitelisted_caller();
+        let module = include_bytes!("../tests/assets/move/build/move/bytecode_scripts/empty_scr.mv").to_vec();
         #[extrinsic_call]
-        execute(RawOrigin::Signed(caller));
+        execute(RawOrigin::Signed(caller), module, 100_000);
+    }
+
+    #[benchmark]
+    fn publish_module() {
+        let caller: T::AccountId = whitelisted_caller();
+        let module = include_bytes!("../tests/assets/move/build/move/bytecode_modules/Empty.mv").to_vec();
+        #[extrinsic_call]
+        publish_module(RawOrigin::Signed(caller), module, 500_000);
     }
 
     impl_benchmark_test_suite!(MovePallet, crate::mock::new_test_ext(), crate::mock::Test);
