347a1e87fc5306349002fe1906dbcb28e43cb4b4
Piotr Olszewski <asmie@asmie.pl>
2023-08-02 11:40:26 +0200

    doc: grammar correction

--- rpc/runtime-api/src/lib.rs
+++ rpc/runtime-api/src/lib.rs
@@ -20,10 +20,10 @@ sp_api::decl_runtime_apis! {
         // Estimate gas for execute script.
         fn estimate_gas_execute(account: AccountId, bytecode: Vec<u8>, gas_limit: u64) -> u64;
 
-        // Get module binary by it's address
+        // Get module binary by its address
         fn get_module(module_id: Vec<u8>) -> Result<Option<Vec<u8>>, Vec<u8>>;
 
-        // Get module ABI by it's address
+        // Get module ABI by its address
         fn get_module_abi(module_id: Vec<u8>) -> Result<Option<Vec<u8>>, Vec<u8>>;
 
         // Get resource
