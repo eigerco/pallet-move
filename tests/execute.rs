5201eb9d0c65d9048e0fd4c190365956956d133a
Piotr Olszewski <asmie@asmie.pl>
2023-08-08 19:25:32 +0200

    test: add integration test stubs
    Integration tests added - only stubs right now.
    Integration tests are failing now and will be changed to real ones
    after extrinsics implementation.

---
+++ tests/execute.rs
@@ -0,0 +1,43 @@
+mod mock;
+use mock::*;
+
+#[test]
+/// Test execution of a script with correct parameters.
+fn execute_script_correct() {
+    new_test_ext().execute_with(|| {
+        assert_eq!(1, 0);
+    });
+}
+
+#[test]
+/// Test execution of a script with correct parameters which stores something inside the storage.
+fn execute_script_storage_correct() {
+    new_test_ext().execute_with(|| {
+        assert_eq!(1, 0);
+    });
+}
+
+#[test]
+/// Test execution of a script with correct parameters but as a wrong user.
+/// Transaction does not require sudo but call was signed with sudo.
+fn execute_script_wrong_user() {
+    new_test_ext().execute_with(|| {
+        assert_eq!(1, 0);
+    });
+}
+
+#[test]
+/// Test execution of a script with correct parameters but with insufficient gas.
+fn execute_script_insufficient_gas() {
+    new_test_ext().execute_with(|| {
+        assert_eq!(1, 0);
+    });
+}
+
+#[test]
+/// Test execution of a script with corrupted bytecode.
+fn execute_script_corrupted_bytecode() {
+    new_test_ext().execute_with(|| {
+        assert_eq!(1, 0);
+    });
+}
