cccf1c350ca37b4168c7e19d322061a0dca0d56a
Ivan Temchenko <35359595i@proton.me>
2023-11-14 16:02:19 -0800

    feat(OC): initial tests and imports

--- tests/execute.rs
+++ tests/execute.rs
@@ -1,6 +1,13 @@
 mod mock;
 use mock::*;
 use move_core_types::account_address::AccountAddress;
+use sp_core::offchain::{testing, OffchainWorkerExt, TransactionPoolExt};
+use sp_keystore::{testing::MemoryKeystore, Keystore, KeystoreExt};
+use sp_runtime::KeyTypeId;
+
+const MOVE: &str = "0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22";
+const PHRASE: &str = "news slush supreme milk chapter athlete soap sausage put clutch what kitten";
+const TEST_ID: KeyTypeId = KeyTypeId([1u8; 4]);
 
 #[test]
 #[ignore = "to be implemented"]
@@ -51,10 +58,28 @@ fn execute_script_corrupted_bytecode() {
 #[test]
 fn round_conversion_native_move_works() {
     new_test_ext().execute_with(|| {
-        const MOVE: &str = "0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22";
         let native =
             MoveModule::move_to_native(&AccountAddress::from_hex_literal(MOVE).unwrap()).unwrap();
         let move_again = MoveModule::native_to_move(&native).unwrap();
         assert_eq!(MOVE, move_again.to_hex_literal());
     })
+}
+
+#[test]
+fn offline_client_bad_inputs_emmits_correct_error_events() {
+    let (offchain, offchain_state) = testing::TestOffchainExt::new();
+    let (pool, pool_state) = testing::TestTransactionPoolExt::new();
+
+    let keystore = MemoryKeystore::new();
+
+    keystore
+        .sr25519_generate_new(TEST_ID, Some(&format!("{}/hunter1", PHRASE)))
+        .unwrap();
+
+    let public_key = *keystore.sr25519_public_keys(TEST_ID).get(0).unwrap();
+    let mut t = sp_io::TestExternalities::default();
+    t.register_extension(OffchainWorkerExt::new(offchain));
+    t.register_extension(TransactionPoolExt::new(pool));
+    t.register_extension(KeystoreExt::new(keystore));
+    new_test_ext().execute_with(|| {});
 }
