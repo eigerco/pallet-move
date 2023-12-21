457ad834e0f746bc113b24da6bc9093bc5ba2cb3
Karlo <88337245+Rqnsom@users.noreply.github.com>
2023-12-21 20:50:03 +0100

    feat(test): add all publish/bundle tests

--- tests/publish.rs
+++ tests/publish.rs
@@ -31,29 +31,53 @@ fn publish_module_as_user_correct() {
 }
 
 #[test]
-#[ignore = "to be implemented"]
 /// Test that the module is not published if the user is not the owner.
 fn publish_module_as_user_wrong_user() {
     new_test_ext().execute_with(|| {
-        assert_eq!(1, 0);
+        let module = assets::read_module_from_project("move-basics", "Empty");
+
+        let res = MoveModule::publish_module(
+            RuntimeOrigin::signed(BOB_ADDR_NATIVE.clone()),
+            module,
+            MAX_GAS_AMOUNT,
+        );
+        assert!(res.is_err());
     });
 }
 
 #[test]
-#[ignore = "to be implemented"]
 /// Test that the module is not published if the user does not have enough gas.
 fn publish_module_as_user_insufficient_gas() {
     new_test_ext().execute_with(|| {
-        assert_eq!(1, 0);
+        let module = assets::read_module_from_project("move-basics", "Empty");
+
+        let gas_limit = 1;
+        let res = MoveModule::publish_module(
+            RuntimeOrigin::signed(CAFE_ADDR_NATIVE.clone()),
+            module,
+            gas_limit,
+        );
+        assert!(res.is_err());
     });
 }
 
 #[test]
-#[ignore = "to be implemented"]
 /// Test that the module is not published if the bytecode is corrupted.
 fn publish_module_as_user_corrupted_bytecode() {
     new_test_ext().execute_with(|| {
-        assert_eq!(1, 0);
+        let mut module = assets::read_module_from_project("move-basics", "Empty");
+
+        // This should be enough to corrupt the bytecode.
+        for i in 0..(module.len() / 4) {
+            module[i] += 1;
+        }
+
+        let res = MoveModule::publish_module(
+            RuntimeOrigin::signed(CAFE_ADDR_NATIVE.clone()),
+            module,
+            MAX_GAS_AMOUNT,
+        );
+        assert!(res.is_err());
     });
 }
 
@@ -75,29 +99,58 @@ fn publish_bundle_as_user_correct() {
 }
 
 #[test]
-#[ignore = "to be implemented"]
 /// Test that the bundle is not published if the user is not the owner.
 fn publish_bundle_as_user_wrong_user() {
     new_test_ext().execute_with(|| {
-        assert_eq!(1, 0);
+        let bundle =
+            assets::read_bundle_from_project("using_stdlib_natives", "using_stdlib_natives");
+
+        let res = MoveModule::publish_module_bundle(
+            RuntimeOrigin::signed(CAFE_ADDR_NATIVE.clone()),
+            bundle,
+            MAX_GAS_AMOUNT,
+        );
+
+        assert!(res.is_err());
     });
 }
 
 #[test]
-#[ignore = "to be implemented"]
 /// Test that the bundle is not published if the user does not have enough gas.
 fn publish_bundle_as_user_insufficient_gas() {
     new_test_ext().execute_with(|| {
-        assert_eq!(1, 0);
+        let bundle =
+            assets::read_bundle_from_project("using_stdlib_natives", "using_stdlib_natives");
+
+        let gas_limit = 1;
+        let res = MoveModule::publish_module_bundle(
+            RuntimeOrigin::signed(BOB_ADDR_NATIVE.clone()),
+            bundle,
+            gas_limit,
+        );
+
+        assert!(res.is_err());
     });
 }
 
 #[test]
-#[ignore = "to be implemented"]
 /// Test that the bundle is not published if the bytecode is corrupted.
 fn publish_bundle_as_user_corrupted_bytecode() {
     new_test_ext().execute_with(|| {
-        assert_eq!(1, 0);
+        let mut bundle =
+            assets::read_bundle_from_project("using_stdlib_natives", "using_stdlib_natives");
+
+        // This should be enough to corrupt the bytecode.
+        for i in 0..(bundle.len() / 4) {
+            bundle[i] += 1;
+        }
+
+        let res = MoveModule::publish_module_bundle(
+            RuntimeOrigin::signed(CAFE_ADDR_NATIVE.clone()),
+            bundle,
+            MAX_GAS_AMOUNT,
+        );
+        assert!(res.is_err());
     });
 }
 
