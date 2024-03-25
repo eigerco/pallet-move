965fa246ab40c2f5affe9ab63c4f8fccba6a6432
Michael Eberhardt <github@michael-eberhardt.de>
2024-03-25 10:09:41 +0100

    chore: update helper script parameters in example move project

--- pallet/src/tests/assets/move-projects/multiple-signers/estimate.sh
+++ pallet/src/tests/assets/move-projects/multiple-signers/estimate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+cd $(dirname $0)
+# Our students who want to rent a dorm together.
 ALICE=5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
 CHARLIE=5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y
 EVE=5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw
-
-STUDENTS=5D6pX37Vm8umrtaBah3q7unW3zuatQ3wJrpsXVsi8xxeBJgB
-
+# Estimate needed gas for a single script execution.
 smove node rpc estimate-gas-execute-script -a $ALICE -s build/multiple-signers/script_transactions/rent_apartment.mvt --cheque-limit 100000000000000
 
